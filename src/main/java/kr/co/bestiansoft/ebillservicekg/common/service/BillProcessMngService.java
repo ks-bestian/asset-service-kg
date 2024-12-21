5070c99ec683b65a7405785c5b14b2c16b204025
cbk <hoffnan@naver.com>
2024-12-21 14:44:11 +0900

    delete common

--- src/main/java/kr/co/bestiansoft/ebillservicekg/common/service/BillProcessMngService.java
+++
@@ -1,8 +0,0 @@
-package kr.co.bestiansoft.ebillservicekg.common.service;
-import kr.co.bestiansoft.ebillservicekg.bill.review.billMng.vo.BillProcMngVo;
-
-public interface BillProcessMngService {
-
-	BillProcMngVo makeProc(String procKingCd,String billId);
-
-}
