d62842c2798b46b4cba535a7e62fda49ca3dccf9
cbk <hoffnan@naver.com>
2024-12-29 11:47:05 +0900

    dd

--- src/main/java/kr/co/bestiansoft/ebillservicekg/process/vo/ProcessVo.java
+++ src/main/java/kr/co/bestiansoft/ebillservicekg/process/vo/ProcessVo.java
@@ -6,4 +6,26 @@ import lombok.Data;
 @Data
 public class ProcessVo extends ComDefaultVO {
 
+	private String bpDfId;
+	private String bpDfNm;
+	private String rmk;
+	private String bpInstanceId;
+	private String billId;
+	private String startDt;
+	private String endDt;
+	private String status;
+	private String currentStepId;
+	private String stepId;
+	private String stepNm;
+	private String nextStepId;
+	private String stepType;
+	private String reqAuthId;
+	private String stepOrd;
+	private String taskId;
+	private String taskNm;
+	private String assignedTo;
+	private String dueDt;
+	private String completedDt;
+	private String billAprvNo;
+
 }
