b3879c467db1b5a08cba9ca08e48b153ef8c5daa
chojh1222 <chojh1222@naver.com>
2025-01-07 17:04:32 +0900

    로그인시 권한조회, 배치

---
+++ src/main/java/kr/co/bestiansoft/ebillservicekg/common/file/service/impl/FileScheduledTasks.java
@@ -0,0 +1,35 @@
+package kr.co.bestiansoft.ebillservicekg.common.file.service.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import kr.co.bestiansoft.ebillservicekg.common.file.service.ComFileService;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class FileScheduledTasks {
+
+	private static final Logger logger = LoggerFactory.getLogger(FileScheduledTasks.class);
+	private final ComFileService storageService;
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Autowired
+	private EDVHelper edv;
+
+	//@Scheduled(cron = "0 0/5 * * * ?") // 매 5분마다
+    ///@Scheduled(fixedRate = 300000) // 300000 밀리초 = 5분  5 60 1000
+    @Scheduled(fixedRate = 3600000) // 300000 밀리초 = 5분  5 60 1000  60 60 1000
+    public void edvFileDeleteTask() {
+    	String formattedDate = dateFormat.format(new Date(System.currentTimeMillis()));
+        logger.info("배치 작업이 실행됩니다: " + formattedDate);
+        storageService.batchFileDelete();
+        logger.info("배치 작업이 종료됩니다: " + formattedDate);
+    }
+}
