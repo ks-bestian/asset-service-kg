1f0cfa46e67147b2f7ae59076a5157cf8ea57466
qyjean <99635933+qyjean@users.noreply.github.com>
2025-01-17 13:41:18 +0900

    '게시판'

--- src/main/java/kr/co/bestiansoft/ebillservicekg/admin/bbs/repository/BoardMapper.java
+++ src/main/java/kr/co/bestiansoft/ebillservicekg/admin/bbs/repository/BoardMapper.java
@@ -9,7 +9,7 @@ import kr.co.bestiansoft.ebillservicekg.admin.bbs.vo.BoardVo;
 @Mapper
 public interface BoardMapper {
     List<BoardVo> getBoardList (HashMap<String, Object> param);
-    BoardVo getBoardById(Long id);
+    BoardVo getBoardById(Long id, String lang);
     void deleteById(Long id);
     int updateById(BoardVo boardVo);
 
