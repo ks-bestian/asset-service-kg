07c34c4e42ea807d8b8ff090ddf69d026b262bcd
cbk <hoffnan@naver.com>
2025-04-08 10:54:02 +0900

    pdf

--- src/main/java/kr/co/bestiansoft/ebillservicekg/common/file/service/impl/PdfServiceImpl.java
+++ src/main/java/kr/co/bestiansoft/ebillservicekg/common/file/service/impl/PdfServiceImpl.java
@@ -1,22 +1,17 @@
 package kr.co.bestiansoft.ebillservicekg.common.file.service.impl;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.aspose.cells.Workbook;
 import com.aspose.slides.Presentation;
 import com.aspose.words.Document;
+import com.aspose.words.PdfFontEmbeddingMode;
+import com.aspose.words.PdfImageCompression;
+import com.aspose.words.PdfSaveOptions;
 import com.aspose.words.SaveFormat;
 
 import kr.co.bestiansoft.ebillservicekg.common.file.service.PdfService;
@@ -26,28 +21,28 @@ public class PdfServiceImpl implements PdfService {
 
 	@Autowired
 	private EDVHelper edv;
-	
+
 	@Autowired
 	private ExecutorService executorService;
-	
+
 	@Override
 	public void convertDocToPdf(String docfilepath, String pdffilepath) throws Exception {
 		Document doc = new Document(docfilepath);
 		doc.save(pdffilepath, SaveFormat.PDF);
 	}
-	
+
 	@Override
 	public void convertPptToPdf(String pptfilepath, String pdffilepath) throws Exception {
 		Presentation pres = new Presentation(pptfilepath);
 		pres.save(pdffilepath, com.aspose.slides.SaveFormat.Pdf);
 	}
-	
+
 	@Override
 	public void convertXlsToPdf(String xlsfilepath, String pdffilepath) throws Exception {
 		Workbook book = new Workbook(xlsfilepath);
 		book.save(pdffilepath, com.aspose.cells.SaveFormat.PDF);
 	}
-	
+
 	@Override
 	public boolean convertToPdf(String filepath, String filename, String pdffilepath) throws Exception {
 		if (filename.endsWith(".doc") || filename.endsWith(".docx")) {
@@ -64,4 +59,51 @@ public class PdfServiceImpl implements PdfService {
 		}
 		return false;
 	}
+
+
+
+
+	public static void main(String args[])  {
+
+		//String docfilepath = "d:/imsi/25MB.doc";
+		String docfilepath = "d:/imsi/1717.docx";
+		String pdffilepath = "d:/imsi/output_low.pdf";
+
+		Document doc;
+
+		System.out.println("start");
+
+		try {
+			doc = new Document(docfilepath);
+
+			// 최적화
+//			doc.cleanup();
+//			doc.joinRunsWithSameFormatting();
+//			doc.updatePageLayout();
+
+//			PdfSaveOptions options = new PdfSaveOptions();
+//			options.setImageCompression(PdfImageCompression.JPEG);
+//			options.setJpegQuality(60);
+//			options.setEmbedFullFonts(false);
+//			options.setFontEmbeddingMode(PdfFontEmbeddingMode.EMBED_ALL);
+			//options.setTempFolder(new File("/tmp/aspose-temp"));
+			doc.save(pdffilepath, SaveFormat.PDF);
+
+			//doc.save(pdffilepath, options);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.toString());
+		}
+
+
+
+
+		System.out.println("end");
+
+
+	}
+
+
 }
