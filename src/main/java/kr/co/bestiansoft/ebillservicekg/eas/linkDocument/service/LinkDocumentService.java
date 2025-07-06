27015a9712077dbb2b23f9cb7c4286ae80d03d8c
범수 <delt96@naver.com>
2025-07-06 21:52:53 +0900

    2025-07-06[update] 이행자 수정

--- src/main/java/kr/co/bestiansoft/ebillservicekg/eas/linkDocument/service/LinkDocumentService.java
+++ src/main/java/kr/co/bestiansoft/ebillservicekg/eas/linkDocument/service/LinkDocumentService.java
@@ -1,5 +1,6 @@
 package kr.co.bestiansoft.ebillservicekg.eas.linkDocument.service;
 
+import kr.co.bestiansoft.ebillservicekg.eas.linkDocument.vo.LinkDocumentListDto;
 import kr.co.bestiansoft.ebillservicekg.eas.linkDocument.vo.LinkDocumentVo;
 
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.List;
 public interface LinkDocumentService {
     int insertLinkDocument(LinkDocumentVo vo);
     int deleteLinkDocument(String fromDocId, String toDocId);
-    List<LinkDocumentVo> getLinkDocumentByDocId(String docId);
+    List<LinkDocumentListDto> getLinkDocumentByDocId(String docId);
     LinkDocumentVo getLinkDocumentByDocIdAndType(String docId, String linkType);
     List<LinkDocumentVo> getLinkDocument(int linkId);
 }
